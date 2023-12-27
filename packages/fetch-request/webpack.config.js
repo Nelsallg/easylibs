28c83143a0d2b1ac829a25c367d7faf4660de5bc
nelsallg <nels.holy.allg@gmail.com>
2023-12-28 00:52:12 +0300

    first commit'

--- packages/fetch-request/webpack.config.js
+++ packages/fetch-request/webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: {
-    fetch_request: './src/fetch-request.ts',
+    "fetch-request": './src/fetch-request.ts',
   },
   output: {
     filename: '[name].js',
