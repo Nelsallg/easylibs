07249214508ff5b1ce4e87c5a30da2f17e38e26b
nelsallg <nels.holy.allg@gmail.com>
2023-12-29 10:20:16 +0300

    fisrt commit

---
+++ packages/file-uploader/dist/@types/file-uploader.d.ts
@@ -0,0 +1,32 @@
+declare type FileUploaderType = Array<{
+    name: string;
+    base64String: string | null;
+    contentType: string;
+    size: number;
+    arrayBuffer: ArrayBuffer;
+}> | {
+    name: string;
+    base64String: string | null;
+    contentType: string;
+    size: number;
+    arrayBuffer: ArrayBuffer;
+};
+export default class FileUploader {
+    private input;
+    private fileElement;
+    private autoEvent;
+    private progressContainer?;
+    /**
+     * @param input La balise input qui recevra l'image
+     * @param fileElement La balise dans laquelle l'image sera affiché
+     */
+    constructor(input: HTMLInputElement | string, fileElement?: HTMLImageElement | string | null, autoEvent?: boolean, progressContainer?: HTMLElement | string);
+    load<T extends FileUploaderType>(callback?: (files: T) => void): void;
+    private singleUploading;
+    private multipleUploading;
+    autoUploadFile(): void;
+    progress(file: File): void;
+    private progressHTML;
+    private createInput;
+}
+export {};
